113aab7f69ced031dfc982504f95d83caf0feb96
Vladimir Parfinenko <vladimir.parfinenko@gmail.com>
2024-08-25 09:23:25 +0200

    Second commit
    * require src argument
    * get rid of mode
    * add timestamp to preview & out name
    * reuse source name for out
    * prepend few frames to out before motion detected

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 Using python with cv2 for motion detection and record only motion detected frames into a video file.
 To use:
 - Create virtual env for python
-- Install requirements `pip install -r /requirements.txt`
-- Run `python ./motion_detect.py -m dir -s  [Path to source files]`
+- Install requirements `pip install -r ./requirements.txt`
+- Run `python ./motion_detect.py -s [Path to source files] {-d [Path to output files]}`
 
--- motion_detect.py
+++ motion_detect.py
@@ -1,17 +1,29 @@
+import os
 import imutils
 import cv2
 import numpy as np
 import time
 from glob import glob
+from collections import deque
 
 # motion detection is comparision between previous_frame && current_frame
 
 FRAMES_TO_PERSIST = 10 # Updates the previous frame in every 10th frame from the loop.
-MIN_SIZE_FOR_MOVEMENT = 200 # higher is the number lesser is motion detection sensitivity. (window size)
-MOVEMENT_DETECTED_PERSISTENCE = 100 # no. of frame count down before saving the video.
+MIN_SIZE_FOR_MOVEMENT = 1200 # higher is the number lesser is motion detection sensitivity. (window size)
+MOVEMENT_DETECTED_PERSISTENCE = 20 # no. of frame count down before saving the video.
+
+
+def millis_to_str(millis):
+    # Convert milliseconds to timestamp
+    seconds = millis // 1000
+    millis %= 1000
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    return "%d:%02d:%02d.%03d" % (hours, minutes, seconds, millis)
 
 
 def get_movement_segments(source: str, processed_files_path: str = './recordings'):
+    source_basename = os.path.splitext(os.path.basename(source))[0]
     
     cap = cv2.VideoCapture(source) # Then start the webcam
     frame_rate = int(cap.get(cv2.CAP_PROP_FPS))
@@ -25,16 +37,22 @@ def get_movement_segments(source: str, processed_files_path: str = './recordings
     delay_counter = 0
     movement_persistent_counter = 0
 
-    fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
-    fourcc = cv2.VideoWriter_fourcc(*'h264') # .mp4
-
+    #fourcc = cv2.VideoWriter_fourcc(*'mjpg')
+    #fourcc = cv2.VideoWriter_fourcc(*'h264') # .mp4, doesn't work on macOS for original frame size
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v') # .mp4, works on macOS
     # fourcc = cv2.VideoWriter_fourcc(*'XVID') # .avi
+
     out = None
+
+    # They are used as pre-frames to captured motion.
+    # Their number is limited.
+    prev_frames = deque(maxlen=MOVEMENT_DETECTED_PERSISTENCE)
 
     while True:
         transient_movement_flag = False    
         ret, frame = cap.read()
-        text = "Unoccupied"
+
+        millis = int(cap.get(cv2.CAP_PROP_POS_MSEC))
         
         if not ret:
             print("CAPTURE ENDED")
@@ -71,42 +89,52 @@ def get_movement_segments(source: str, processed_files_path: str = './recordings
         thresh = cv2.dilate(thresh, None, iterations = 2)
         cnts, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-        for c in cnts:
-            (x, y, w, h) = cv2.boundingRect(c)
-            if cv2.contourArea(c) > MIN_SIZE_FOR_MOVEMENT:
+        max_contour = max(cnts, key=cv2.contourArea, default=None)
+        if max_contour is not None:
+            # Draw max contour rect.
+            (x, y, w, h) = cv2.boundingRect(max_contour)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+            # Detect movement only if area is big enough.
+            max_contour_area = cv2.contourArea(max_contour)
+            if max_contour_area > MIN_SIZE_FOR_MOVEMENT:
                 transient_movement_flag = True
-                # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        else:
+            max_contour_area = 0
+
 
         if transient_movement_flag == True:
             movement_persistent_flag = True
             movement_persistent_counter = MOVEMENT_DETECTED_PERSISTENCE
 
+        debug_text = f"counter {str(movement_persistent_counter).zfill(2)} movement area {str(int(max_contour_area)).zfill(5)}"
         if movement_persistent_counter > 0:
-            text = "Movement Detected " + str(movement_persistent_counter)
             movement_persistent_counter -= 1
-        else:
-            text = "No Movement Detected"
 
-        # cv2.putText(frame, str(text), (10,35), font, 0.75, (255,255,255), 2, cv2.LINE_AA)
+        cv2.putText(frame, f"{source_basename} @ {millis_to_str(millis)}", (10,35), font, 0.75, (255,255,255), 2, cv2.LINE_AA)
+        cv2.putText(frame, debug_text, (10,65), font, 0.75, (255,255,255), 2, cv2.LINE_AA)
         frame_delta = cv2.cvtColor(frame_delta, cv2.COLOR_GRAY2BGR)
         # cv2.imshow("frame", np.hstack((frame_delta, frame)))
         cv2.imshow("frame", frame)
 
 
         # record video -----------
-        if movement_persistent_counter>0 and not out: # for the very first frame
-            height, width, _ = frame.shape
-            out = cv2.VideoWriter(f'{processed_files_path}/{int(time.time())}_video.mp4',fourcc, frame_rate ,(width, height))
-            out.write(frame)
-
-        if movement_persistent_counter>0 and out:
-            out.write(frame)
+        if movement_persistent_counter>0:
+            if not out: # for the very first frame
+                height, width, _ = original_frame.shape
+                timestamp = millis_to_str(millis).replace(':', '-').replace('.', '-')
+                out = cv2.VideoWriter(f'{processed_files_path}/{source_basename}_{timestamp}_{int(time.time())}.mp4',
+                                      fourcc, frame_rate, (width, height))
+                for f in prev_frames:
+                    out.write(f)
+            out.write(original_frame)
 
         # Save each clip in separate video file
-        if movement_persistent_counter == 0 and out:
+        elif movement_persistent_counter == 0 and out:
             out.release()
             out = None
-        
+
+        prev_frames.append(original_frame)
 
         ch = cv2.waitKey(1)
         if ch & 0xFF == ord('q'):
@@ -126,24 +154,25 @@ def get_movement_segments(source: str, processed_files_path: str = './recordings
     return
 
 
-def process_pipeline(source_path: str, destination_path: str = './recordings', mode: str ='dir'):
-    if mode == 'dir':
-        print("start processing")
-        sources = glob(source_path + "*.[mM][pP]4") # change to needed 
-        print(sources)
+def process_pipeline(source_path: str, destination_path: str = './recordings'):
+    if os.path.isdir(source_path):
+        sources = glob(source_path + "/*.[mM][pP]4") # change to needed
+        print(f"start processing: {sources}")
         for source in sources:
             print(source)
             get_movement_segments(source, destination_path)
-    elif mode=='file': 
+    elif os.path.isfile(source_path):
+        print(f"start processing: {source_path}")
         get_movement_segments(source_path, destination_path)
+    else:
+        print(f"{source_path} is neither a file nor a directory.")
     exit()
 
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
     parser = ArgumentParser()
-    parser.add_argument("-s", "--source_dir", type=str)
+    parser.add_argument("-s", "--source_dir", type=str, required=True)
     parser.add_argument("-d", "--destination_dir", type=str, default='./recordings')
-    parser.add_argument("-m", "--mode", type=str, default="dir", choices=['dir', 'file'])
     args = parser.parse_args()
-    process_pipeline(args.source_dir, args.destination_dir, args.mode)
+    process_pipeline(args.source_dir, args.destination_dir)
